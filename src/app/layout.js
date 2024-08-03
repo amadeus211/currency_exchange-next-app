47e03079f019aa563c73b01dc9aa83ed1c1268b2
amadeus211 <malyuk2004.mail.ru@gmail.com>
2024-08-03 15:02:38 +0300

    change title

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,13 +3,16 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Create Next App",
+  title: "Currency exchange",
+  
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {" "}
+      <head>
+        <link rel="icon" href="/currency.png" type="image/png" />
+      </head>
       <body className="font-extralight">{children}</body>
     </html>
   );
